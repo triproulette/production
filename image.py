52ce90595737d420d2146217f86299e2aa65b047
amit sandak <sandak.amit@gmail.com>
2016-04-27 22:06:33 +0300

    Merge branch 'master' of https://github.com/triproulette/production fdf # Conflicts: #	app.py
    # Conflicts:
    #	some_page.py

--- image.py
+++ image.py
@@ -7,12 +7,12 @@ import falcon
 
 class Resource(object):
 
-      def on_get(self, req, resp, name):
+      def on_get(self, req, resp):
         try:
-            resp.content_type = mimetypes.guess_type(name)[0]
-            image_path = os.path.join(self.storage_path, name)
-            resp.stream = open('pics\triprollete.jpg', 'rb')
-            resp.stream_len = os.path.getsize('pics\triprollete.jpg')
+            resp.content_type = mimetypes.guess_type('a')[0]
+            image_path = os.path.join(self.storage_path, 'a')
+            resp.stream = open('pics\\triprollete.jpg', 'rb')
+            resp.stream_len = os.path.getsize('pics\\triprollete.jpg')
         except Exception as e:
             resp.body = '{"message": %s}' % e.message
             resp.status = falcon.HTTP_200
