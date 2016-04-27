26544bc66873e9dc9d1148149c43f4436db0d45a
amit sandak <sandak.amit@gmail.com>
2016-04-27 21:52:26 +0300

    Merge branch 'master' of https://github.com/triproulette/production fdf # Conflicts: #	app.py
    # Conflicts:
    #	some_page.py

--- image.py
+++ image.py
@@ -1,8 +1,48 @@
+
+import os
+import uuid
+import mimetypes
+
 import falcon
+
+
+class Collection(object):
+
+    def __init__(self, storage_path):
+        self.storage_path = 'triprollete.jpg'
+
+    def on_post(self, req, resp):
+        ext = mimetypes.guess_extension(req.content_type)
+        filename = '{uuid}{ext}'.format(uuid=uuid.uuid4(), ext=ext)
+        image_path = os.path.join(self.storage_path, filename)
+
+        with open(image_path, 'wb') as image_file:
+            while True:
+                chunk = req.stream.read(4096)
+                if not chunk:
+                    break
+
+                image_file.write(chunk)
+
+        resp.status = falcon.HTTP_201
+        resp.location = '/image/' + filename
+
+
+class Item(object):
+
+    def __init__(self, storage_path):
+        self.storage_path = storage_path
+
+    def on_get(self, req, resp, name):
+        resp.content_type = mimetypes.guess_type(name)[0]
+        image_path = os.path.join(self.storage_path, name)
+        resp.stream = open('triprollete.jpg', 'rb')
+        resp.stream_len = os.path.getsize('triprollete.jpg')
+
 
 
 class Resource(object):
 
     def on_get(self, req, resp):
-        resp.body = '{"message": "code updated!!!"}'
+        resp.body =
         resp.status = falcon.HTTP_200
