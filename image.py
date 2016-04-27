82f5e9fe5ee45a97861ea0aada3c9da114deb09c
Bar David <bardavvid@gmail.com>
2016-04-27 22:17:03 +0300

    fixed image py

--- image.py
+++ image.py
@@ -10,9 +10,8 @@ class Resource(object):
       def on_get(self, req, resp):
         try:
             resp.content_type = mimetypes.guess_type('a')[0]
-            image_path = os.path.join('//pics//', 'a')
-            resp.stream = open('logo.JPG', 'rb')
-            resp.stream_len = os.path.getsize('logo.JPG')
+            resp.stream = open('pics/logo.JPG', 'rb')
+            resp.stream_len = os.path.getsize('pics/logo.JPG')
         except Exception as e:
             resp.body = '{"error": %s}' % e.message
             resp.status = falcon.HTTP_200
