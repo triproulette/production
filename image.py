d8574568807a9ba77f90960049d7cb7003b17048
Bar David <bardavvid@gmail.com>
2016-04-27 22:10:53 +0300

    fixed image py

--- image.py
+++ image.py
@@ -10,7 +10,7 @@ class Resource(object):
       def on_get(self, req, resp):
         try:
             resp.content_type = mimetypes.guess_type('a')[0]
-            image_path = os.path.join('pics\\', 'a')
+            image_path = os.path.join('//pics//', 'a')
             resp.stream = open('triprollete.jpg', 'rb')
             resp.stream_len = os.path.getsize('triprollete.jpg')
         except Exception as e:
