0a17c2d7560df7351aff0e96e3061b4bf7b38143
amit sandak <sandak.amit@gmail.com>
2016-04-27 22:09:16 +0300

    Merge branch 'master' of https://github.com/triproulette/production fdf # Conflicts: #	app.py
    # Conflicts:
    #	some_page.py

--- image.py
+++ image.py
@@ -14,7 +14,7 @@ class Resource(object):
             resp.stream = open('pics\\triprollete.jpg', 'rb')
             resp.stream_len = os.path.getsize('pics\\triprollete.jpg')
         except Exception as e:
-            resp.body = '{"message": %s}' % e.message
+            resp.body = '{"error": %s}' % e.message
             resp.status = falcon.HTTP_200
 
 
