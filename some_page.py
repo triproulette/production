0dbb006e7f88f3657bf571f4925ca81beae87115
Bar David <bardavvid@gmail.com>
2016-04-27 20:59:47 +0300

    beep test 222

--- some_page.py
+++ some_page.py
@@ -4,7 +4,7 @@ import falcon
 class Resource(object):
 
     def on_get(self, req, resp):
-        resp.body = '{"message": "!azxaazfafa!@kod! up!dated!!!!!!"}'
+        resp.body = '{"message": "!azx2aazfafa!@kod! up!dated!!!!!!"}'
         resp.status = falcon.HTTP_200
 #sandak
 #Tomer
