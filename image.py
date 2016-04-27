f05d6e9c50cd7f2d0889bb9fe8fbee9b45857e4c
amit sandak <sandak.amit@gmail.com>
2016-04-27 21:58:39 +0300

    Merge branch 'master' of https://github.com/triproulette/production fdf # Conflicts: #	app.py
    # Conflicts:
    #	some_page.py

--- image.py
+++ image.py
@@ -5,37 +5,11 @@ import mimetypes
 
 import falcon
 
-
-class Collection(object):
-
-    def __init__(self, storage_path):
-        self.storage_path = 'triprollete.jpg'
-
-    def on_post(self, req, resp):
-        ext = mimetypes.guess_extension(req.content_type)
-        filename = '{uuid}{ext}'.format(uuid=uuid.uuid4(), ext=ext)
-        image_path = os.path.join(self.storage_path, filename)
-
-        with open(image_path, 'wb') as image_file:
-            while True:
-                chunk = req.stream.read(4096)
-                if not chunk:
-                    break
-
-                image_file.write(chunk)
-
-        resp.status = falcon.HTTP_201
-        resp.location = '/image/' + filename
-
-
 class Resource(object):
 
-    def __init__(self, storage_path):
-        self.storage_path = storage_path
-
-    def on_get(self, req, resp, name):
+      def on_get(self, req, resp, name):
         resp.content_type = mimetypes.guess_type(name)[0]
         image_path = os.path.join(self.storage_path, name)
-        resp.stream = open('triprollete.jpg', 'rb')
-        resp.stream_len = os.path.getsize('triprollete.jpg')
+        resp.stream = open('pics\triprollete.jpg', 'rb')
+        resp.stream_len = os.path.getsize('pics\triprollete.jpg')
 
