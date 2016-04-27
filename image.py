4086922a90bb27b13a10231fec28b924389b3c34
Bar David <bardavvid@gmail.com>
2016-04-27 22:05:32 +0300

    fixed image py

--- image.py
+++ image.py
@@ -8,8 +8,13 @@ import falcon
 class Resource(object):
 
       def on_get(self, req, resp, name):
-        resp.content_type = mimetypes.guess_type(name)[0]
-        image_path = os.path.join(self.storage_path, name)
-        resp.stream = open('pics\triprollete.jpg', 'rb')
-        resp.stream_len = os.path.getsize('pics\triprollete.jpg')
+        try:
+            resp.content_type = mimetypes.guess_type(name)[0]
+            image_path = os.path.join(self.storage_path, name)
+            resp.stream = open('pics\triprollete.jpg', 'rb')
+            resp.stream_len = os.path.getsize('pics\triprollete.jpg')
+        except Exception as e:
+            resp.body = '{"message": %s}' % e.message
+            resp.status = falcon.HTTP_200
+
 
