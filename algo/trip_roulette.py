550c6481aa7068c3e9e424831f8421fb3230a19c
Bar David <bardavvid@gmail.com>
2016-05-06 21:27:22 +0300

    template generate

--- algo/trip_roulette.py
+++ algo/trip_roulette.py
@@ -1,15 +1,25 @@
 from algo.template_roulette import TemplateRoulette
+from Entities.Trip import Trip
+from Entities.Event import Event
+from datetime import datetime
 
 
 class TripRoulette(object):
 
-    def __init__(self, trip_type):
+    def __init__(self, trip_type, city, start_trip, end_trip):
         self.trip_type = trip_type
+        self.city = city
+        self.start_trip = start_trip
+        self.end_trip = end_trip
 
     def generate_random_trip(self):
-        self.prop = TemplateRoulette(self.trip_type).generate_template()
-
-
-trip = TripRoulette(trip_type="Travel")
-trip.generate_random_trip()
-print trip.prop
+        self._prop = TemplateRoulette(self.trip_type).generate_template()   # generate template
+        self._trip = Trip(tripID=0,)  # generate trip entity
+        self._trip._eventList.append(Event(eventID=0,
+                                           startTime=datetime(self.start_trip),
+                                           endTime=datetime(self.start_trip),
+                                           poiID=0))     # append first event
+        self._trip._eventList.append(Event(eventID=99,
+                                           startTime=datetime(self.end_trip),
+                                           endTime=datetime(self.end_trip),
+                                           poiID=1))    # append last event
