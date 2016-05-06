48928e0f434ba0a09e9fe64b3c03b408a96dca9f
Bar David <bardavvid@gmail.com>
2016-05-06 13:24:06 +0300

    api handler push

---
+++ APIHandler/abs_api_handler_test.py
@@ -0,0 +1,7 @@
+from abs_api_handler import APIHandler
+
+test = APIHandler("https://api.sandbox.amadeus.com/v1.2/points-of-interest/yapq-search-circle/?")
+test.serialize_params({"apikey": "qvXIKasoUoG7AysZpsjURU01XSlsu2kb", "latitude": "35.1504", "longitude": "-114.57632",
+                       "radius": "42"})
+res = test.call_api()
+print res
