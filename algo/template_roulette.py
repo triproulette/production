503e0b6000a954bce417989ece32ee1bf981b0f4
Bar David <bardavvid@gmail.com>
2016-05-06 19:49:01 +0300

    template generate

--- algo/template_roulette.py
+++ algo/template_roulette.py
@@ -1,4 +1,5 @@
 from time import time
+from Entities.Properties import Properties
 
 
 class TemplateRoulette(object):
@@ -6,3 +7,7 @@ class TemplateRoulette(object):
     def __init__(self, trip_type):
         self.trip_type = trip_type
 
+    def generate_template(self):
+        prop = Properties(dayBeginning=time(8,0,0),dayEnd=time(20,0,0),breakfastTime=time(9,0,0),lunchTime=time(13,0,0),
+                          dinnerTime=time(19,0,0),type=self.trip_type)
+        return prop
