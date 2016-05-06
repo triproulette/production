5768a92d6f4e9560a02859ce3689df2a554fa07f
Bar David <bardavvid@gmail.com>
2016-05-06 14:42:35 +0300

    google api

---
+++ APIHandler/google_distance_api.py
@@ -0,0 +1,14 @@
+from abs_api_handler import APIHandler
+
+
+class GoogleDistance(APIHandler):
+
+    def __init__(self):
+        self.apikey = "AIzaSyA9UwHPOJ3JLbnrK8P3wgcSWLXgCXOCkFo"
+        APIHandler.__init__(self, "https://maps.googleapis.com/maps/api/distancematrix/json?")
+
+    def call_api(self, o_latitude, o_longitude, d_latitude, d_longitude):
+        params = {"apikey": self.apikey, "origins": str(o_latitude) + "," + str(o_longitude),
+                  "destinations": str(d_latitude) + "," + str(d_longitude)}
+        APIHandler.serialize_params(self, params)
+        return APIHandler.call_api(self)
