9d874f58afcaf12014e0abd711f0e6c07ce34436
Bar David <bardavvid@gmail.com>
2016-05-06 14:20:48 +0300

    api handler push

--- APIHandler/tests/hotel_geosearch_api_test.py
+++ APIHandler/tests/hotel_geosearch_api_test.py
@@ -2,6 +2,6 @@ from APIHandler.hotel_geosearch_api import HotelGeosearch
 
 
 test = HotelGeosearch()
-results = test.call_api(latitude=32.1504, longitude=-114.57632, radius=42,
+results = test.call_api(latitude=33.1504, longitude=-114.57632, radius=42,
                         check_in_date="2016-06-14", check_out_date="2016-06-16")
 print results
