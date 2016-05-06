a025efde7edb584ab6abb2dd51225239e03f9cc7
Bar David <bardavvid@gmail.com>
2016-05-06 19:14:23 +0300

    hotel api entity connect

--- Entities/Event.py
+++ Entities/Event.py
@@ -10,7 +10,7 @@ class Event(object):
         self._endTime=endTime
 
     def printEvent(self):
-        print("\n Event ID:%d" % self._eventID+"\n poi ID:%d" % self._poiID+"\n start time:" , self._startTime , "\n end time:" , self._endTime, "\n **************")
+        print("\n Eventt ID:%d" % self._eventID+"\n poi ID:%d" % self._poiID+"\n start time:" , self._startTime , "\n end time:" , self._endTime, "\n **************")
 
 #Test for print;
 #event1=Event(1,1,startTime=datetime.datetime(2016,5,6,12,40,0),endTime=datetime.datetime(2016,5,6,12,40,0))
