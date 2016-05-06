f89a347b1b7c8109e4a6c680f93ca520de2cdd19
Bar David <bardavvid@gmail.com>
2016-05-07 01:12:21 +0300

    first DB try

--- some_page.py
+++ some_page.py
@@ -8,7 +8,7 @@ class Resource(object):
     def on_get(self, req, resp):
         conn = DBConnection.DBConnection()
         handler = DBHandler.DBHandler(conn)
-        tripId = handler.create(table_name='trip',table_fields="'user_id'",table_values="'some_user_id'")
+        tripId = handler.create(table_name='trip',table_fields='user_id',table_values="'some_user_id'")
         resp.body = '{"message": "trip inserted, id is {}"}'.format(tripId)
         resp.status = falcon.HTTP_200
 #sandak
