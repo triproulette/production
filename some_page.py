2a994c3c8996be25c335658d6189137ba10a4b88
Bar David <bardavvid@gmail.com>
2016-05-07 00:26:46 +0300

    first DB try

--- some_page.py
+++ some_page.py
@@ -9,7 +9,7 @@ class Resource(object):
         #conn = DBConnection.DBConnection()
         #handler = DBHandler.DBHandler(conn)
         #tripId = handler.create(table_name='Trip',table_fields='user_id',table_values='some_user_id')
-        resp.body = '{"message": "trip inserted, id is {}"}'.format('bla')
+        resp.body = '{"message": "trip inserted, id is {}"}'.format('blaa')
         resp.status = falcon.HTTP_200
 #sandak
 #Tomer
