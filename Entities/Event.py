377050a5d250d3f8e57adc5cca68b03db4750ab9
TomKizaru <remot.tomer@gmail.com>
2016-05-06 15:00:14 +0300

    Entities v1.04 | minor fixes

--- Entities/Event.py
+++ Entities/Event.py
@@ -6,20 +6,16 @@ class Event(object):
     _poiID=0
     _startTime=datetime.datetime(2016,5,6,12,40,0)
     _endTime=datetime.datetime(2016,5,6,12,40,0)
-    _prevEvent=0
-    _nextEvent=0
 
-    def __init__(self,eventID,poiID,startTime,endTime,prevEvent,nextEvent):
+    def __init__(self,eventID,poiID,startTime,endTime):
         self._eventID=eventID
         self._poiID=poiID
         self._startTime=startTime
         self._endTime=endTime
-        self._prevEvent=prevEvent
-        self._nextEvent=nextEvent
 
     def printEvent(self):
-        print("\n Event ID:%d" % self._eventID+"\n poi ID:%d" % self._poiID+"\n start time:" , self._startTime , "\n end time:" , self._endTime , "\n prev event:%d" % self._prevEvent+"\n next event:%d" % self._nextEvent)
+        print("\n Event ID:%d" % self._eventID+"\n poi ID:%d" % self._poiID+"\n start time:" , self._startTime , "\n end time:" , self._endTime, "\n **************")
 
 #Test for print;
-#event1=Event(1,1,startTime=datetime.datetime(2016,5,6,12,40,0),endTime=datetime.datetime(2016,5,6,12,40,0),prevEvent=0,nextEvent=2)
+#event1=Event(1,1,startTime=datetime.datetime(2016,5,6,12,40,0),endTime=datetime.datetime(2016,5,6,12,40,0))
 #event1.printEvent()
--- Entities/Trip.py
+++ Entities/Trip.py
@@ -35,8 +35,8 @@ class Trip(object):
 
 #Testing of setEventList with int List; Works by index :)
 Trip1=Trip(1,9,9,500)
-Trip1.setEventList(0,Event(2,3,startTime=datetime.datetime(2016,5,6,12,40,0),endTime=datetime.datetime(2016,5,6,12,40,0),prevEvent=1,nextEvent=3))
+Trip1.setEventList(0,Event(2,3,startTime=datetime.datetime(2016,5,6,12,40,0),endTime=datetime.datetime(2016,5,6,12,40,0)))
 Trip1.printTrip()
-event1= Event(3,54,startTime=datetime.datetime(2016,5,6,12,45,35),endTime=datetime.datetime(2016,5,6,12,46,44),prevEvent=2,nextEvent=4)
+event1= Event(3,54,startTime=datetime.datetime(2016,5,6,12,45,35),endTime=datetime.datetime(2016,5,6,12,46,44))
 Trip1.updateEventList(event1)
 Trip1.printTrip()
