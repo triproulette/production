55aec35487660b81c96d1c8f685cf7120ad10c8c
Bar David <bardavvid@gmail.com>
2016-05-06 15:35:14 +0300

    google api

--- APIHandler/tests/poi_geosearch_api_test.py
+++ APIHandler/tests/poi_geosearch_api_test.py
@@ -1,6 +1,12 @@
 from APIHandler.poi_geosearch_api import POIGeosearch
+from Entities.POI import POI
 
 
 test = POIGeosearch()
-results = test.call_api(latitude=35.1504, longitude=-114.57632, radius=42)
-print results
+results = test.call_api(latitude=52.374320, longitude=4.822390, radius=100)
+pois = []
+
+for poi in results["points_of_interest"]:
+    print poi["title"]
+    exit()
+    pois.append(POI(poiID=0,title=poi["title"], description=,category=,grade=poi["grades"]['yapq_grade']))
