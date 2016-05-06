2e74e6b4ffc63149620596bd08db777091f46436
Bar David <bardavvid@gmail.com>
2016-05-06 17:25:06 +0300

    hotel api entity connect

--- APIHandler/tests/hotel_geosearch_api_test.py
+++ APIHandler/tests/hotel_geosearch_api_test.py
@@ -2,6 +2,8 @@ from APIHandler.hotel_geosearch_api import HotelGeosearch
 
 
 test = HotelGeosearch()
-results = test.call_api(latitude=34.1504, longitude=-114.57632, radius=42,
+results = test.call_api(latitude=32.096306, longitude=34.783167, radius=100,
                         check_in_date="2016-06-14", check_out_date="2016-06-16")
-print results
+
+for p in results:
+    print(p)
