2f62df2ebea2abb70172ed2f05f76a785269ae59
AsafKaravani <asaf.karavani1@gmail.com>
2016-05-06 18:41:13 +0300

    DBHandler - initial commit

---
+++ DBHandler/DBHandler.py
@@ -0,0 +1,36 @@
+import mysql.connector
+
+class DBHandler:
+    db_connection = None
+
+    def __init__(self, db_connection):
+        self.db_connection = db_connection
+
+    def __print__(self):
+        print("User: " + self.db_connection.user)
+        print("Password: " + self.db_connection.password)
+        print("Host: " + self.db_connection.host)
+        print("DB: " + self.db_connection.database)
+
+
+    def execute_query(self, query):
+        m = self.db_connection.connection.cursor()
+        m.execute(query)
+        return m.fetchall()
+
+
+    def create_user(self):
+
+    def read_user(self):
+
+    def update_user(self):
+
+    def delete_user(self):
+
+
+
+
+
+
+
+
