23caf245826e5941c7335ce16284419ae60b9773
Bar David <bardavvid@gmail.com>
2016-05-07 08:15:11 +0300

    FIrst jquery integration try

--- trip.py
+++ trip.py
@@ -12,6 +12,8 @@ class Resource(object):
             handler = DBHandler.DBHandler(conn)
             results = handler.query("SELECT p.title,p.description,p.category,e.s_time, e.e_time "+
                                     "FROM tr_db.poi as p JOIN tr_db.event as e ON e.poi_id = p.id")
-            print results
+            conn.close()
+
+            print results[0][0], results[0][1]
             resp.body = '{msg: "bla"}'
             resp.status = falcon.HTTP_200
