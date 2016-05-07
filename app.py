c27350eab874dc7f7472d5ef70940d2fc395cac1
Bar David <bardavvid@gmail.com>
2016-05-07 12:54:10 +0300

    trip backend try

--- app.py
+++ app.py
@@ -3,12 +3,16 @@ import some_page
 import image
 import index
 import trip
+import user
 
 
 api = application = falcon.API()
 
 api.add_route('/some_page', some_page.Resource())
 api.add_route('/image', image.Resource())
-api.add_route('/trip', index.Resource())
-api.add_route('/trip/{trip_id}/{action}/{event_id}', trip.Resource())
-api.add_route('/user/{action}/{id}/{fname}/{lname}/{avatar}', trip.Resource())
+
+trip = trip.Resource()
+
+api.add_route('/trip/{action}/{user_id}', trip)
+api.add_route('/trip/{trip_id}/{action}/{event_id}', trip)
+api.add_route('/user/{action}/{id}/{fname}/{lname}/{avatar}', user.Resource())
--- trip.py
+++ trip.py
@@ -2,6 +2,8 @@ import falcon
 import json
 from DBHandler import DBHandler
 from DBHandler import DBConnection
+from algo.trip_roulette import TripRoulette
+import datetime
 
 
 class Resource(object):
@@ -29,4 +31,14 @@ class Resource(object):
                     continue
 
             resp.body = '{{ events: [ {} ] }}'.format(', '.join(json_event))
+            resp.status = falcon.HTTP_200
+
+    def on_get(self, req, resp, action, user_id):
+
+        if action == 'generate':
+
+            generator = TripRoulette(0, "paris", datetime(2016,05,07,8,0,0), datetime(2016,05,12,21,0,0))
+            generator.generate_random_trip()
+
+            resp.body = ''
             resp.status = falcon.HTTP_200
