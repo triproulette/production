06fbae93f5da411835c674e77fccea1137378d9a
Bar David <bardavvid@gmail.com>
2016-05-07 15:06:04 +0300

    trip backend try

--- poi.py
+++ poi.py
@@ -12,6 +12,6 @@ class Resource(object):
             if city == 'amsterdam':
                 print 'amsterdam'
                 poi_generator = POIGeosearch()
-                results = poi_generator.call_api(latitude=52.310558, longitude=4.768221, radius=100)
+                results = poi_generator.call_api(latitude=52.310558, longitude=4.768221, radius=50)
                 resp.body = results
                 resp.status = falcon.HTTP_200
