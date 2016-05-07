b219174433296dca11122cec07aa3e33ac137541
Bar David <bardavvid@gmail.com>
2016-05-07 09:58:47 +0300

    trip backend try

--- trip.py
+++ trip.py
@@ -29,6 +29,8 @@ class Resource(object):
                     json_event.append(ev)
                 except:
                     print 'error appending'
+                    print '{}'.format(len(results[i]))
+                    print '{}'.format(results[i])
                     continue
 
             resp.body = '{{ events: [ {} ] }}'.format(', '.join(json_event))
