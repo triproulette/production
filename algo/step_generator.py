cd3c5ee21b8644a47082d46f1426de66e1f2ba88
amit sandak <sandak.amit@gmail.com>
2016-05-07 09:54:12 +0300

    Merge branch 'master' of https://github.com/triproulette/production fdf # Conflicts: #	app.py
    # Conflicts:
    #	some_page.py

--- algo/step_generator.py
+++ algo/step_generator.py
@@ -1,24 +1,45 @@
 from APIHandler.google_distance_api import GoogleDistance
 from APIHandler.hotel_geosearch_api import HotelGeosearch
+from APIHandler.poi_geosearch_api import POIGeosearch
 from DBHandler import DBHandler
 from Entities import POI
 from Entities import Event
 
 import datetime
 
-def stepGenerator(prop,prevnt):
-    db = DBHandler.DBHandler()
-    ##normalization
+class POIGrade(object):
+
+    def __init__(self, grade, poi):
+        self._grade = grade
+        self._poi = poi
+
+
+
+def algo_grade_calc(db, prop, prennt, prev_poi, curevnt):
+    curr_poi = db.getPoi(curevnt.eventID)
     api = GoogleDistance()
-    prev_poi = db.getPoi(prevnt.eventID)
+    distance = api.call_api(prev_poi._geoLocation._latitude, prev_poi._geoLocation._longitude,
+                            curr_poi._geoLocation._latitude, curr_poi._geoLocation._longitude)
 
     if distance > 210:
         distance_grade = 0
     else:
-        distance_grade = distance/210
+        distance_grade = distance / 210
+
+    population_grade = curr_poi._grade / 5;
+
+    grade = population_grade * distance_grade
+
+    return grade
+
+
+def stepGenerator(prop,prevnt):
+    db = DBHandler.DBHandler()
+    ##normalization
+    prev_poi = db.getPoi(prevnt.eventID)
 
     ##prop calc
-##
+
     if abs(prevnt.endTime-prop.breakfastTime) < datetime.timedelta(days=0, hours=1, minutes=0, seconds=0, microseconds=0) :
         foodPoi = POI.POI()
         foodPoi._title = "breakfast"
@@ -69,18 +90,13 @@ def stepGenerator(prop,prevnt):
     if abs(prevnt.endTime - prop._dayEnd) < datetime.timedelta(days=0, hours=1, minutes=0, seconds=0, microseconds=0):
        api = HotelGeosearch()
        results = api.call_api(prev_poi._geoLocation._latitude , prev_poi._geoLocation._longitude, radius=100, check_in_date=prevnt._endtime.isoformat(), check_out_date= prevnt._endtime + datetime.datetime.timedelta(days=1).replace(prop._dayBeginning)).save_to_db()
+######## continue the hotels
+    api = POIGeosearch()
+    results = api.call_api(prev_poi._geoLocation._latitude, prev_poi._geoLocation._longitude, radius=100)
+    calculated = []
+    for poi in results:
+        calculated.append( POIGrade(algo_grade_calc(db, prop, prevnt, prev_poi, poi), poi))
 
-
-    def algo_grade_calc(prop,prennt, curevnt):
-        curr_poi = db.getPoi(curevnt.eventID)
-        distance = api.call_api(prev_poi._geoLocation._latitude, prev_poi._geoLocation._longitude,
-                                curr_poi._geoLocation._latitude, curr_poi._geoLocation._longitude)
-
-        if distance > 210:
-            distance_grade = 0
-        else:
-            distance_grade = distance / 210
-
-        population_grade = curr_poi._grade / 5;
-
-        return grade
+    heapq.heapify(calculated)
+    heapq._heapify_max(calculated)
+    return heapq.heappop()
