1550bb5baf79b57160ee2188c2dddb1a0d118504
Bar David <bardavvid@gmail.com>
2016-05-07 08:33:17 +0300

    FIrst jquery integration try

--- trip.py
+++ trip.py
@@ -8,7 +8,7 @@ class Resource(object):
     def on_get(self, req, resp, trip_id, action, event_id):
 
         if action == 'get_event':
-            ev_count = 4 #  4 events each request
+            ev_count = 4    # 4 events each request
 
             conn = DBConnection.DBConnection()
             handler = DBHandler.DBHandler(conn)
@@ -18,10 +18,12 @@ class Resource(object):
                                     "ORDER BY e.id ASC LIMIT {},{}".format(event_id,ev_count))
             conn.close()
 
+            print results
+
             json_event = []
             for i in range(ev_count):
-                json_event.append('{id: {}, title: {}, description: {}, category: {}, start_time: {}, end_time: {}}'.format(
-                    results[i][0],results[i][1],results[i][2],results[i][3],results[i][4],results[i][5]
+                json_event.append('{id: {}, title: {}, description: {}, category: {}, start_time: {}, end_time: {}}'
+                    .format(results[i][0],results[i][1],results[i][2],results[i][3],results[i][4],results[i][5]
                 ))
 
             resp.body = '{events: [ {},{},{},{} ]}'.format(json_event[0],json_event[1],json_event[2],json_event[3])
